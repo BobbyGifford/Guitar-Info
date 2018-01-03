cc6e9d6f815c8e53a625da4b82d5859e5b350343
Bobby <bobby077joe@gmail.com>
2018-01-02 17:22:32 -0800

    Polish content and optimize code.

--- src/components/about.js
+++ src/components/about.js
@@ -8,30 +8,23 @@ const about = props => {
       </div>
 
       <div className="row">
-        <p style={{ fontFamily: "Raleway" }} className="col align-self-center lead">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-          elementum metus in enim consectetur, et consequat dui imperdiet. Nam
-          condimentum purus vitae posuere tempus. Nullam vel suscipit purus, vel
-          pretium velit. Morbi vel dolor lorem. Proin felis ipsum, porta a
-          dapibus eget, tincidunt sed tortor. Pellentesque non libero gravida
-          massa maximus semper. Donec sit amet mollis mi.
+        <p className="col align-self-center lead">
+          <hr />
+          Playing guitar has been one of my favorite hobbies for about a decade.
+          When I first started playing I didn’t understand what any of the
+          technical specs meant. If you go guitar shopping online you see the
+          same strange looking specs listed such as: scale length, radius, fret
+          size, etc. I made this site to explain what these specs mean to the
+          average person.
         </p>
       </div>
 
       <div className="row">
         <p className="col align-self-center lead">
-          Mauris eget ante eget libero molestie fermentum. Curabitur quis
-          pulvinar libero. Sed vestibulum, tellus congue eleifend dignissim,
-          libero mi lobortis mauris, in varius mauris urna ac felis. Mauris nisl
-          lacus, tristique et est at, congue porttitor metus. In tempus volutpat
-          euismod. Mauris dapibus orci nec mi semper, et dapibus elit
-          pellentesque. Duis enim ex, scelerisque eget metus ut, ullamcorper
-          consectetur magna. Mauris placerat non dolor nec mollis. Phasellus
-          laoreet condimentum odio, at consequat risus. Pellentesque porttitor
-          libero et lacus pharetra, vel consectetur enim elementum. Aenean eu
-          orci quam. Sed aliquam ante urna, quis hendrerit sapien rhoncus eget.
-          Nulla pharetra, tortor ut suscipit rhoncus, diam nibh dapibus justo,
-          eget pretium enim massa et nisi.
+          Guitar specs can make or break guitar playing for most people. I
+          didn’t like the feeling of my guitar when I started. Knowing guitar
+          specs could make guitar playing infinitely easier for people just
+          starting.
         </p>
       </div>
     </div>
