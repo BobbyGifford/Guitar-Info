049481d0f7fefd4ba9731fb7e814cf1869e4f589
Bobby <bobby077joe@gmail.com>
2018-02-05 11:01:51 -0800

    Added intro screen buttons

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import React from "react";
 
 const welcome = () => {
-    return (<div className="welcome-page"><h1 className="welcome-text">this is welcome</h1></div>)
-}
+  return (
+    <div className="welcome-page">
+      <div className="welcome-text">
+        <h1>Welcome to Guitar Info</h1>
+        <button className="btn btn-info">Neck Specs</button>
+        <br />
+        <button className="mt-2 btn btn-info">Body Specs</button>
+      </div>
+    </div>
+  );
+};
 
 export default welcome;
