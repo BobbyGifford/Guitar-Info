1b1af3ebe1f5ddd9af27b5a2fdce6e2b084c88ec
Bobby <bobby077joe@gmail.com>
2018-02-05 13:41:39 -0800

    Add intro screen navigation.

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -1,13 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const welcome = () => {
   return (
     <div className="welcome-page">
       <div className="welcome-text">
         <h1>Welcome to Guitar Info</h1>
-        <button className="btn btn-info">Neck Specs</button>
+        <Link to={"/home"}>
+          <button className="btn btn-success">View All</button>
+        </Link>
         <br />
-        <button className="mt-2 btn btn-info">Body Specs</button>
+        <Link to={"listOf/neck"}>
+          <button className="my-2 btn btn-success">View Neck Specs</button>
+        </Link>
+        <br />
+        <Link to={"listOf/body"}>
+          <button className="btn btn-success">View Body Specs</button>
+        </Link>
       </div>
     </div>
   );
